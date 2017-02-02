e3470ccdd377718922ce1e9aff09f7d1b3cc16cc
vivekkumar11432 <vivekgari@gmail.com>
2017-02-02 17:54:35 +0530

    I Commit

---
+++ EasyPayJS.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        if ($(document).width() > 768) {
+            scroll_pos = $(this).scrollTop();
+            if (scroll_pos > 20) {
+                $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+            } else {
+                $("header").css('background-color', 'transparent');
+            }
+        } else {
+            $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+        }
+        if (checkVisible($('#imageOneh1'))) {
+            $("#imageOne").addClass("animaImg");
+        } else {
+            $('#imageOne').removeClass('animaImg');
+        }
+        if (checkVisible($('#imageTwoh1'))) {
+            $("#imageTwo").addClass("animaImg");
+        } else {
+            $('#imageTwo').removeClass('animaImg');
+        }
+        if (checkVisible($('#imageThreeh1'))) {
+            $("#imageThree").addClass("animaImg");
+        } else {
+            $('#imageThree').removeClass('animaImg');
+        }
+    });
+    $(window).resize(function () {
+        if ($(document).width() > 768) {
+            scroll_pos = $(this).scrollTop();
+            if (scroll_pos > 20) {
+                $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+            } else {
+                $("header").css('background-color', 'transparent');
+            }
+        }
+        else {
+            $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+        }
+    });
+    function checkVisible(elm, eval) {
+        eval = eval || "visible";
+        var vpH = $(window).height(), // Viewport Height
+            st = $(window).scrollTop(), // Scroll Top
+            y = $(elm).offset().top - 77,
+            elementHeight = $(elm).height();
+        if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+        if (eval == "above") return ((y < (vpH + st)));
+    }
+});
